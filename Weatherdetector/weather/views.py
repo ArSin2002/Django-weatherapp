d5aebe61ef51fc07519b668437090c85af1688dc
Aryan Sinha <97186458+ArSin2002@users.noreply.github.com>
2023-06-19 01:44:33 +0530

    added try except block

--- Weatherdetector/weather/views.py
+++ Weatherdetector/weather/views.py
@@ -6,17 +6,31 @@ import urllib.request
 def index(request):
     if request.method == 'POST':
         city=request.POST['city']
-        res=urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=2d6483f5b29bbb3f4fa98d13a5b98c2e').read()
-        json_data=json.loads(res)
+        try:
+            res=urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=2d6483f5b29bbb3f4fa98d13a5b98c2e').read()
+        except Exception as e:
+            city='Enter a valid City or Country'
+            data={
+                'country_code' : 'Invalid',
+                'coordinate' : 'Invalid' ,
+                'temp' : 'Invalid',
+                'pressure' : 'Invalid',
+                'humidity' : "Invalid",
 
-        data={
-            'country_code' : str(json_data['sys']['country']),
-            'coordinate' : str(json_data['coord']['lon']) + ' ' + str(json_data['coord']['lat']) ,
-            'temp' : str(json_data['main']['temp']) + 'k',
-            'pressure' : str(json_data['main']['pressure']) + 'hPa',
-            'humidity' : str(json_data['main']['humidity'])+ '%',
+            }
+            return render(request,'index.html',{'city':"Enter valid City",'data':data})
+            # print('Enter a valid city name:')
+        else:
+            json_data=json.loads(res)
 
-        }
+            data={
+                'country_code' : str(json_data['sys']['country']),
+                'coordinate' : str(json_data['coord']['lon']) + ' ' + str(json_data['coord']['lat']) ,
+                'temp' : str(json_data['main']['temp']) + 'k',
+                'pressure' : str(json_data['main']['pressure']) + 'hPa',
+                'humidity' : str(json_data['main']['humidity'])+ '%',
+
+                }
 
     else:
         data = {}   
